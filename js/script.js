4f2889e5cc875fc2f7ae061abea77a3bad5cac39
Shmiterling <99955098+Shmiterling@users.noreply.github.com>
2022-03-12 16:00:57 +0100

    1

---
+++ js/script.js
@@ -0,0 +1,54 @@
+console.log(`Warsztat pobiranie danych jQuery`)
+$(document).ready(function () {
+
+  $('#btn').click(function () {
+
+    // wariant 1.
+    // $.get('https://akademia108.pl/api/ajax/get-post.php')
+    //   .done(function (data) {
+
+    //     let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+    //     let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+    //     let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+    //     let pBody = $('<p></p>').text(`Body: ${data.body}`);
+    //     let hr = $('<hr>');
+
+    //     $('body').append(pId);
+    //     $('body').append(pUserId);
+    //     $('body').append(pTitle);
+    //     $('body').append(pBody);
+    //     $('body').append(hr);
+
+    //   })
+    //   .fail(function (error) {
+    //     console.error(error);
+    //   })
+
+
+    //wariant 2.
+    $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+      .done(function (data) {
+
+        let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+        let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+        let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+        let pBody = $('<p></p>').text(`Body: ${data.body}`);
+        let hr = $('<hr>');
+
+        $('body').append(pId);
+        $('body').append(pUserId);
+        $('body').append(pTitle);
+        $('body').append(pBody);
+        $('body').append(hr);
+
+      })
+      .fail(function (error) {
+        console.error(error);
+      })
+
+
+
+  })
+
+})
+
